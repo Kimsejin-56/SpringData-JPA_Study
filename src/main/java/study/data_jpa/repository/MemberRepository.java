06af33c8c1a6d9a50534ee003b4102cc267f5e5d
Kimsejin-56 <saj5877@naver.com>
2026-01-30 20:09:12 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -8,8 +8,18 @@ import study.data_jpa.entity.Member;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    /**
+     * 간단한 방식 - 메서드 이름으로 쿼리 생성
+     * 권장 X - NamedQuery
+     * 권장 O + 복잡한 방식 - Query로 레포지토리 메서드에 쿼리 정의
+     */
+
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-    @Query(name = "Member.findByUsername") // 생략 가능 우선순위가 Named 쿼리 찾아서 실행 후에 없으면 메서드 이름으로 쿼리 생성
+    @Query(name = "Member.findByUsername")
+        // 생략 가능 우선순위가 Named 쿼리 찾아서 실행 후에 없으면 메서드 이름으로 쿼리 생성
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findMember(@Param("username") String username, @Param("age") int age);
 }
--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -85,5 +85,18 @@ class MemberRepositoryTest {
 
         assertThat(findMember).isEqualTo(member1);
     }
+
+    @Test
+    public void testFindMember() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findMember("AAA", 10);
+        Member findMember = result.get(0);
+
+        assertThat(findMember).isEqualTo(member1);
+    }
 }
 
