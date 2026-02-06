b0aeec03a708b77d9a30e88141269be36c4a8bbf
Kimsejin-56 <saj5877@naver.com>
2026-02-06 14:57:27 +0900

    스프링 데이터 JPA Auditing

--- src/main/java/study/data_jpa/DataJpaApplication.java
+++ src/main/java/study/data_jpa/DataJpaApplication.java
@@ -1,13 +1,26 @@
 package study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 원래: 세션 정보나, 스프링 시큐리티 로그인 정보에서 ID를 받음
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
