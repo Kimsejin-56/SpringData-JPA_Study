6745201c6370f14a9a67a4a161fc2967e26a36a1
Kimsejin-56 <saj5877@naver.com>
2026-02-07 17:07:29 +0900

    페이징과 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,10 +2,15 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -26,7 +31,7 @@ public class MemberController {
     /**
      * HTTP 요청은 회원 id 를 받지만 도메인 클래스 컨버터가 중간에 동작해서 회원 엔티티 객체를 반환
      * 도메인 클래스 컨버터도 리파지토리를 사용해서 엔티티를 찾음
-     *  - 조회용으로만 사용 (애초에 권장 X)
+     * - 조회용으로만 사용 (애초에 권장 X)
      */
     @GetMapping("/members2/{id}")// 도메인 클래스 컨버터 사용후
     public String findMember2(@PathVariable("id") Member member) {
@@ -35,6 +40,34 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("UserA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("User" + i));
+
+        }
+    }
+
+    /**
+     * - 페이징과 정렬
+     * page: 현재 페이지, 0부터 시작한다.
+     * size: 한 페이지에 노출할 데이터 건수
+     * sort: 정렬 조건을 정의한다.
+     * 예) 정렬 속성,정렬 속성...(ASC | DESC), 정렬 방향을 변경하고 싶으면 sort 파라미터 추가 ( asc 생략 가능)
+     */
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    //개별 설정(기본값)
+    @GetMapping("/members_page")
+    public Page<Member> list2(@PageableDefault(size = 12, sort = "username",
+            direction = Sort.Direction.DESC) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    //Page 내용을 DTO로 변환
+    @GetMapping("members_dto")
+    public Page<MemberDto> list3(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(member -> new MemberDto(member));
     }
 }
